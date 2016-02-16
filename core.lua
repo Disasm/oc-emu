857e6f790f2537e34813d1edcfc19161aae88947
Vadim Kaushan <admin@disasm.info>
2016-02-16 23:46:18 +0300

    Send touch events

--- core.lua
+++ core.lua
@@ -22,9 +22,20 @@ local co = coroutine.create(f)
 while true do
     local ev = table.pack(get_event())
     if ev.n > 0 then
+        print(table.unpack(ev))
         local send = false
         if (ev[1] == "key_down") or (ev[1] == "key_up") then
             ev = {ev[1], addr["keyboard"], ev[2], ev[3], "username", n=5}
+            send = true
+        end
+        if (ev[1] == "mouse_down") then
+            local x = math.floor(ev[2] / 8) + 1
+            local y = math.floor(ev[3] / 16) + 1
+            local button = 0
+            if ev[4] == "left" then
+                button = 1
+            end
+            ev = {"touch", addr["screen"], x, y, button, "username", n=6}
             send = true
         end
 
